3696ab9ed64f285f08b0eba7f93932b00c535000
hendrik-spl <145655111+hendrik-spl@users.noreply.github.com>
2024-07-13 15:24:24 +0200

    improved and streamlined notebooks

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,3 +1,71 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+
+def plot_history(comment, history):
+    """
+    Plot training history.
+    
+    Parameters:
+        comment (str): Comment to display in the plot.
+        history (tf.keras.callbacks.History): Training history.
+
+    Returns:
+        None
+    """
+    plt.figure(figsize=(14, 4))
+    plt.suptitle(comment)
+
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'], label='Training Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.show()
+
+    print(f'Best train_accuracy: {np.max(history.history["accuracy"]).round(4)}')
+    print(f'Best train_loss: {np.min(history.history["loss"]).round(4)}')
+    print(f'Best val_accuracy: {np.max(history.history["val_accuracy"]).round(4)}')
+    print(f'Best val_loss: {np.min(history.history["val_loss"]).round(4)}')
+    print(f'Last improvement at epoch: {np.argmax(history.history["val_accuracy"])+1}')
+
+def plot_predictions(model, dataset, dataset_visual, class_names = ["Closed", "Open"], num_images=5):
+    """
+    Plot predictions.
+    
+    Parameters:
+        model (tf.keras.Model): Model to use for predictions.
+        dataset (tf.data.Dataset): Dataset with images.
+        dataset_visual (tf.data.Dataset): Dataset with visual images.
+        class_names (list): Class names. Default is ["Closed", "Open"].
+        num_images (int): Number of images to plot. Default is 5.
+    """
+    plt.figure(figsize=(15, 6))
+    images_displayed = 0
+
+    for (images, labels), (images_visual, _) in zip(dataset.take(1), dataset_visual.take(1)):
+        predictions = model.predict(images)
+        predicted_labels = tf.argmax(predictions, axis=1)
+
+        for i in range(len(images)):
+            if images_displayed >= num_images:
+                break
+
+            plt.subplot(1, num_images, images_displayed + 1)
+            plt.imshow(images_visual[i].numpy().astype("uint8"))
+            plt.title(f"True: {class_names[labels[i]]}\nPred: {class_names[predicted_labels[i]]}")
+            plt.axis("off")
+            images_displayed += 1
+
 def evaluate_model(model, test_data):
     """
     Evaluate the model on the test data.
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import numpy as np
 
 def load_and_preprocess_images(path, batch_size, image_size, seed, data_aug_rate=0, shuffle=True, subset=None, validation_split=None, skip_preprocessing=False):
     """
@@ -58,42 +57,6 @@ def load_and_preprocess_images(path, batch_size, image_size, seed, data_aug_rate
 
     return preprocessed_data.cache().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-def plot_history(comment, history):
-    """
-    Plot training history.
-    
-    Parameters:
-        comment (str): Comment to display in the plot.
-        history (tf.keras.callbacks.History): Training history.
-
-    Returns:
-        None
-    """
-    plt.figure(figsize=(14, 4))
-    plt.suptitle(comment)
-
-    plt.subplot(1, 2, 1)
-    plt.plot(history.history['accuracy'], label='Training Accuracy')
-    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.legend()
-
-    plt.subplot(1, 2, 2)
-    plt.plot(history.history['loss'], label='Training Loss')
-    plt.plot(history.history['val_loss'], label='Validation Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
-
-    plt.show()
-
-    print(f'Best train_accuracy: {np.max(history.history["accuracy"]).round(4)}')
-    print(f'Best train_loss: {np.min(history.history["loss"]).round(4)}')
-    print(f'Best val_accuracy: {np.max(history.history["val_accuracy"]).round(4)}')
-    print(f'Best val_loss: {np.min(history.history["val_loss"]).round(4)}')
-    print(f'Last improvement at epoch: {np.argmax(history.history["val_accuracy"])+1}')
-
 def plot_images(img_orig, img_augm, num_images=5):
     """
     Plot original and augmented images.
@@ -127,32 +90,4 @@ def plot_images(img_orig, img_augm, num_images=5):
             plt.title(f"Augmented")
             plt.axis("off")
 
-            images_displayed += 1
-
-def plot_predictions(model, dataset, dataset_visual, class_names = ["Closed", "Open"], num_images=5):
-    """
-    Plot predictions.
-    
-    Parameters:
-        model (tf.keras.Model): Model to use for predictions.
-        dataset (tf.data.Dataset): Dataset with images.
-        dataset_visual (tf.data.Dataset): Dataset with visual images.
-        class_names (list): Class names. Default is ["Closed", "Open"].
-        num_images (int): Number of images to plot. Default is 5.
-    """
-    plt.figure(figsize=(15, 6))
-    images_displayed = 0
-
-    for (images, labels), (images_visual, _) in zip(dataset.take(1), dataset_visual.take(1)):
-        predictions = model.predict(images)
-        predicted_labels = tf.argmax(predictions, axis=1)
-
-        for i in range(len(images)):
-            if images_displayed >= num_images:
-                break
-
-            plt.subplot(1, num_images, images_displayed + 1)
-            plt.imshow(images_visual[i].numpy().astype("uint8"))
-            plt.title(f"True: {class_names[labels[i]]}\nPred: {class_names[predicted_labels[i]]}")
-            plt.axis("off")
             images_displayed += 1
